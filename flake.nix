7087a7b38423a270c6b7936e7065253a135cca37
cubos-d <ja.cub05.19@gmail.com>
2026-09-04 11:03:51 -0500

    fix: I will not add nixpkgs-unstable yet. Perhaps I will uptate the entire system to unstable

--- flake.nix
+++ flake.nix
@@ -1,11 +1,8 @@
-{ ... }:
-
 {
   description = "Modern Environment for ML projects";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
